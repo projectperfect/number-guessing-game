87575ecf8260b485412f02e3284a5e8939b8561d
projectperfect <jprfct@protonmail.com>
2022-08-07 12:28:00 -0500

    Check answer function.

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 from random import randint
 from art import logo 
+
+
+def check_answer(guess, answer, turns):
+    """Checks answer against guess. Returns the number of turns remaining."""
+    if guess > answer:
+        print("Too high.")
+        return turns - 1
+    elif guess < answer:
+        print("Too low.")
+        return turns - 1
+    else:
+        print(f"You got it! The answer was {answer}.")
+
